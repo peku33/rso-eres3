0dfb5e9b313fcfea27bc3b18a7b1fecf0c20b51f
Grzegorz Majchrzak <gregiem94@gmail.com>
2017-03-16 19:39:26 +0100

    Wprowadzono poprawki:
    ale po zmianie na es6 po uruchomieniu build:prod wyskakuje:
    ERROR in app.bundle.js from UglifyJs
    Unexpected token: name (AppModule) [app.bundle.js:80157,4]

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,11 +1,13 @@
 import 'core-js';
 import 'reflect-metadata';
 import 'zone.js';
-import 'jquery';
+import 'tether';
 import 'bootstrap';
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
 
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
