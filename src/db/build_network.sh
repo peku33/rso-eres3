9c067770afaa7e057dff81d075a560a16b02e6ca
Lukasz Kostyra <costyrra.xl@gmail.com>
2017-05-13 21:57:06 +0200

    NDB Docker image, scripts to setup network for the cluster

---
+++ src/db/build_network.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CLUSTER_NAME="eres3-dbcluster"
+IP_PREFIX="192.168.103"
+
+docker network create $CLUSTER_NAME --subnet=$IP_PREFIX.0/24
+
+docker run -d --net=$CLUSTER_NAME --name=man1.ndb --ip=$IP_PREFIX.201 $CLUSTER_NAME ndb_mgmd
+docker run -d --net=$CLUSTER_NAME --name=man2.ndb --ip=$IP_PREFIX.202 $CLUSTER_NAME ndb_mgmd
+docker run -d --net=$CLUSTER_NAME --name=data1.ndb --ip=$IP_PREFIX.211 $CLUSTER_NAME ndbd
+docker run -d --net=$CLUSTER_NAME --name=data2.ndb --ip=$IP_PREFIX.212 $CLUSTER_NAME ndbd
---
+++ src/db/clean_network.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CLUSTER_NAME="eres3-dbcluster"
+
+declare -a nodes=("data2.ndb" "data1.ndb" "man2.ndb" "man1.ndb")
+
+for i in "${nodes[@]}"
+do
+    docker stop $i
+done
+
+for i in "${nodes[@]}"
+do
+    docker rm $i
+done
+
+docker network rm $CLUSTER_NAME
