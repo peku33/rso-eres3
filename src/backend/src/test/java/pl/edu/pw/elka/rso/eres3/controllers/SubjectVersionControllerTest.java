dd85da72a772d89cc666703804cb1d2d81e159c9
ferwiner2 <ferwiner2@gmail.com>
2017-05-20 11:47:42 +0200

    Moving tests to junit 4

--- src/backend/src/test/java/pl/edu/pw/elka/rso/eres3/controllers/SubjectVersionControllerTest.java
+++ src/backend/src/test/java/pl/edu/pw/elka/rso/eres3/controllers/SubjectVersionControllerTest.java
@@ -1,50 +1,46 @@
 package pl.edu.pw.elka.rso.eres3.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.Collections;
-
+import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
-
-import com.google.common.collect.Lists;
-
-import junit.framework.TestCase;
 import pl.edu.pw.elka.rso.eres3.domain.entities.Subject;
 import pl.edu.pw.elka.rso.eres3.domain.entities.SubjectVersion;
 import pl.edu.pw.elka.rso.eres3.domain.repositories.SubjectVersionRepository;
 
-public class SubjectVersionControllerTest extends TestCase {
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SubjectVersionControllerTest {
 
 	private SubjectVersionRepository mockVersionRepo;
 	private SubjectVersionController controller;
 
-	@Override
 	@Before
-	protected void setUp() throws Exception {
+	public void setUp() throws Exception {
 		mockVersionRepo = mock(SubjectVersionRepository.class);
 		controller = new SubjectVersionController(mockVersionRepo);
 	}
 
 	@Test
-	public void testAddSubjectVerion_noVersionExists_versionCodeIsA() throws Exception {
+	public void testAddSubjectVersion_noVersionExists_versionCodeIsA() throws Exception {
 		//given
 		final Integer subjectId = 1;
 		final SubjectVersion versionToAdd = mockVersion(null, subjectId);
 		when(mockVersionRepo.findBySubjectId(subjectId)).thenReturn(Collections.emptyList());
 		when(mockVersionRepo.save(versionToAdd)).thenReturn(versionToAdd);
 		//when
-		final ResponseEntity<SubjectVersion> response = controller.addSubjectVerion(versionToAdd);
+		final ResponseEntity<SubjectVersion> response = controller.addSubjectVersion(versionToAdd);
 		//then
 		assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
 		assertThat(response.getBody().getVersionCode()).isEqualTo('A');
 	}
 
 	@Test
-	public void testAddSubjectVerion_versionsExist_versionCodeIsIncremented() throws Exception {
+	public void testAddSubjectVersion_versionsExist_versionCodeIsIncremented() throws Exception {
 		//given
 		final Integer subjectId = 1;
 		final SubjectVersion versionToAdd = mockVersion(null, subjectId);
@@ -52,7 +48,7 @@ public class SubjectVersionControllerTest extends TestCase {
 				mockVersion('A', subjectId), mockVersion('C', subjectId)));
 		when(mockVersionRepo.save(versionToAdd)).thenReturn(versionToAdd);
 		//when
-		final ResponseEntity<SubjectVersion> response = controller.addSubjectVerion(versionToAdd);
+		final ResponseEntity<SubjectVersion> response = controller.addSubjectVersion(versionToAdd);
 		//then
 		assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
 		assertThat(response.getBody().getVersionCode()).isEqualTo('D');
