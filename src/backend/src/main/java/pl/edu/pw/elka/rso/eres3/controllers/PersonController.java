9eec1235aca727961f5ba0b1c630dc1281dc93ff
psrok1 <psrok1@gmail.com>
2017-05-28 21:21:13 +0200

    Added mappings to PersonController

--- src/backend/src/main/java/pl/edu/pw/elka/rso/eres3/controllers/PersonController.java
+++ src/backend/src/main/java/pl/edu/pw/elka/rso/eres3/controllers/PersonController.java
@@ -1,15 +1,19 @@
 package pl.edu.pw.elka.rso.eres3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pw.elka.rso.eres3.controllers.abstractions.AbstractCrudController;
 import pl.edu.pw.elka.rso.eres3.domain.entities.Person;
+import pl.edu.pw.elka.rso.eres3.domain.entities.dto.PersonDto;
 import pl.edu.pw.elka.rso.eres3.domain.repositories.PersonRepository;
+import pl.edu.pw.elka.rso.eres3.security.PersonService;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Rest controller for person.
@@ -25,26 +29,33 @@ public class PersonController extends AbstractCrudController<Person, Long> {
 	static final String mapping = "/persons";
 	
 	@Autowired
+	private PersonService personService;
+	
+	@Autowired
 	PersonController(final PersonRepository repository){
 		super(repository, true);
 	}
 
 	@RequestMapping(value = mapping, method = RequestMethod.GET)
     @PreAuthorize("hasPermission(null, 'PersonRead')")
-	public List<Person> getAllPersons() {
-		return getAll();
+	public List<PersonDto> getAllPersons() {
+		return getAll().stream()
+				.map(person -> personService.mapPersonToDto(person))
+				.collect(Collectors.toList());
 	}
 
 	@RequestMapping(value = mapping + "/{id}", method = RequestMethod.GET)
     @PreAuthorize("hasPermission(null, 'PersonRead')")
-	public ResponseEntity<Person> getPerson(@PathVariable final long id) {
-		return getEntity(id);
+	public ResponseEntity<PersonDto> getPerson(@PathVariable final long id) {
+		PersonDto personDto = personService.mapPersonToDto(getEntity(id).getBody());
+		return new ResponseEntity<PersonDto>(personDto, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = mapping, method = RequestMethod.POST)
     @PreAuthorize("hasPermission(null, 'PersonCreate')")
-	public ResponseEntity<Person> addPerson(@RequestBody final Person person) {
-		return addEntity(person);
+	public ResponseEntity<Person> addPerson(@RequestBody final PersonDto personDto) {
+		Person person = personService.registerNewPersonAccount(personDto);
+		return new ResponseEntity<Person>(person, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = mapping, method = RequestMethod.PUT)
