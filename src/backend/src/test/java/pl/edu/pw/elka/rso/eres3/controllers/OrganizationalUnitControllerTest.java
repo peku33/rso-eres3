d14ae12e4d0d02e19f63d186c97514745855a81c
Michał Kosikowski <michalkosikowski@gmail.com>
2017-06-06 16:50:01 +0200

    added unit controller tests + removed prints to console

--- src/backend/src/test/java/pl/edu/pw/elka/rso/eres3/controllers/OrganizationalUnitControllerTest.java
+++ src/backend/src/test/java/pl/edu/pw/elka/rso/eres3/controllers/OrganizationalUnitControllerTest.java
@@ -22,11 +22,10 @@ import org.springframework.web.context.WebApplicationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import pl.edu.pw.elka.rso.eres3.domain.entities.dto.OrganizationalUnitDto;
-import pl.edu.pw.elka.rso.eres3.domain.entities.dto.PersonDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PersonControllerTest {
+public class OrganizationalUnitControllerTest {
 	@Autowired
     private WebApplicationContext wac;
 
@@ -44,9 +43,7 @@ public class PersonControllerTest {
 	
     private MockMvc mockMvc;
     
-    private static final String mapping = "/persons";
-    
-    private static OrganizationalUnitDto unitStub = new OrganizationalUnitDto();
+    private static final String mapping = "/units";
 
     @Before
     public void setup() throws SQLException {
@@ -54,54 +51,43 @@ public class PersonControllerTest {
         
         Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
         Statement stmt = conn.createStatement();
-        String sql = "insert into organizational_unit(id, full_name, short_name) values(1, \"test\", \"test\")";
-        stmt.executeUpdate(sql);
-        sql = "insert into person(login, password, id, unit_id) values(\"Test\", \"Test\", 1,1), (\"Test2\", \"Test\", 2, 1);";
+        String sql = "insert into organizational_unit(id, full_name, short_name) values(1, \"Test\", \"Test\"), (2, \"Test2\", \"Test2\");";
         stmt.executeUpdate(sql);
         conn.close();
-        
-    	unitStub.setFullName("test");
-    	unitStub.setShortName("test");
-    	unitStub.setId((short) 1);
+       
     }
     
    @After
     public void clean() throws SQLException{
     	Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
         Statement stmt = conn.createStatement();
-        String sql = "delete from person where id<4 or login='Test3';";
-        stmt.executeUpdate(sql);
-        sql = "delete from organizational_unit where id=1;";
+        String sql = "delete from organizational_unit where id<3 or short_name='Test3';";
         stmt.executeUpdate(sql);
         conn.close();
     }
     
     @Test
-    public void addPersonTest() throws Exception{
-    	PersonDto personStub = new PersonDto();
-
-    	personStub.setUnit(unitStub);
-    	personStub.setLogin("Test3");
-    	personStub.setPassword("Test");
+    public void addUnitTest() throws Exception{
+    	OrganizationalUnitDto unitStub = new OrganizationalUnitDto();
+    	unitStub.setFullName("Test3");
+    	unitStub.setShortName("Test3");
     	
     	this.mockMvc.perform(post(mapping).contentType(MediaType.APPLICATION_JSON)
-    			.content(objectMapper.writeValueAsString(personStub))
+    			.content(objectMapper.writeValueAsString(unitStub))
     			)
-    	.andExpect(status().isOk());
+    	.andExpect(status().isCreated());
     }
     
     @Test
-    public void updatePersonTest() throws Exception{
+    public void updateUnitTest() throws Exception{
     	String name = "Testo";
-    	PersonDto personStub = new PersonDto();
-    	personStub.setUnit(unitStub);
-    	personStub.setLogin("Test2");
-    	personStub.setPassword("Test");
-    	personStub.setFirstName(name);
-    	personStub.setId(new Long(2));
+    	OrganizationalUnitDto unitStub = new OrganizationalUnitDto();
+    	unitStub.setFullName("Test2");
+    	unitStub.setShortName(name);
+    	unitStub.setId((short) 2);
     	
     	this.mockMvc.perform(put(mapping).contentType(MediaType.APPLICATION_JSON).
-    			content(objectMapper.writeValueAsString(personStub))
+    			content(objectMapper.writeValueAsString(unitStub))
     			)
     	.andExpect(status().isOk());
     	
@@ -109,44 +95,44 @@ public class PersonControllerTest {
     	.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
     	.andReturn().getResponse().getContentAsString();
     	
-    	PersonDto upPerson = objectMapper.readValue(result, PersonDto.class);
-    	if(!upPerson.getFirstName().equals(name)){
+    	OrganizationalUnitDto upUnit = objectMapper.readValue(result, OrganizationalUnitDto.class);
+    	if(!upUnit.getShortName().equals(name)){
     		fail("Update failed!");
     	}
     }
     
     @Test
-    public void getAllStudentsTest() throws Exception{
+    public void getAllUnitsTest() throws Exception{
     	String results = this.mockMvc.perform(get(mapping))
     			.andExpect(status().isOk())
     			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-    			.andExpect(jsonPath("$[?(@.login=='Test2')]").isNotEmpty())
+    			.andExpect(jsonPath("$[?(@.fullName=='Test2')]").isNotEmpty())
     			.andReturn().getResponse().getContentAsString()
     			;
-    	PersonDto[] persons = objectMapper.readValue(results, PersonDto[].class);
+    	OrganizationalUnitDto[] units = objectMapper.readValue(results, OrganizationalUnitDto[].class);
     	long id = 0;
-    	for(PersonDto person: persons){
-    		if(person.getLogin().equals("Test2")){
-    			id = person.getId();
+    	for(OrganizationalUnitDto unit: units){
+    		if(unit.getFullName().equals("Test2")){
+    			id = unit.getId();
     			break;
     		}
     	}
     	if(id==0){
-    		fail("Person not found!");
+    		fail("Unit not found!");
     	}
     }
     
     @Test
-    public void deleteStudentTest() throws Exception{
+    public void deleteUnitTest() throws Exception{
     	this.mockMvc.perform(delete(mapping+"/1")).andExpect(status().isNoContent());
     	
     	String results = this.mockMvc.perform(get(mapping))
     			.andReturn().getResponse().getContentAsString()
     			;
-    	PersonDto[] persons = objectMapper.readValue(results, PersonDto[].class);
-    	for(PersonDto person: persons){
-    		if(person.getLogin().equals("Test")){
-    			fail("Person found after delete!");
+    	OrganizationalUnitDto[] units = objectMapper.readValue(results, OrganizationalUnitDto[].class);
+    	for(OrganizationalUnitDto unit: units){
+    		if(unit.getShortName().equals("Test")){
+    			fail("Unit found after delete!");
     		}
     	}
     		
