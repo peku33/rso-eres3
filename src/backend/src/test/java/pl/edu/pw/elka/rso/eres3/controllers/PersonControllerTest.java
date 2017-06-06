bc433cfedf873c947e58b4dae33c21dea5ce620d
Michał Kosikowski <michalkosikowski@gmail.com>
2017-06-06 16:29:33 +0200

    reworked tests with @Before and @After

--- src/backend/src/test/java/pl/edu/pw/elka/rso/eres3/controllers/PersonControllerTest.java
+++ src/backend/src/test/java/pl/edu/pw/elka/rso/eres3/controllers/PersonControllerTest.java
@@ -4,12 +4,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.junit.Assert.*;
 
-import java.util.Random;
+import java.sql.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -31,27 +33,57 @@ public class PersonControllerTest {
 	@Autowired
 	ObjectMapper objectMapper;
 	
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	
+	@Value("${spring.datasource.username}")
+	private String dbUser;
+	
+	@Value("${spring.datasource.password}")
+	private String dbPass;
+	
     private MockMvc mockMvc;
     
-    static final String mapping = "/persons";
+    private static final String mapping = "/persons";
+    
+    private static OrganizationalUnitDto unitStub = new OrganizationalUnitDto();
 
     @Before
-    public void setup() {
+    public void setup() throws SQLException {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).apply(springSecurity()).build();
-    }
-    
-    @Test
-    public void addPerson() throws Exception{
-    	Random rand = new Random();
-    	String rstring = "test" + new Integer((rand.nextInt())).toString();
-    	PersonDto personStub = new PersonDto();
-    	OrganizationalUnitDto unitStub = new OrganizationalUnitDto();
+        
+        Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+        Statement stmt = conn.createStatement();
+        String sql = "insert into organizational_unit(id, full_name, short_name) values(1, \"test\", \"test\")";
+        stmt.executeUpdate(sql);
+        sql = "insert into person(login, password, id, unit_id) values(\"Test\", \"Test\", 1,1), (\"Test2\", \"Test\", 2, 1);";
+        stmt.executeUpdate(sql);
+        conn.close();
+        
     	unitStub.setFullName("test");
     	unitStub.setShortName("test");
     	unitStub.setId((short) 1);
+    }
+    
+   @After
+    public void clean() throws SQLException{
+    	Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+        Statement stmt = conn.createStatement();
+        String sql = "delete from person where id<4 or login='Test3';";
+        stmt.executeUpdate(sql);
+        sql = "delete from organizational_unit where id=1;";
+        stmt.executeUpdate(sql);
+        conn.close();
+    }
+    
+    @Test
+    public void addPersonTest() throws Exception{
+    	PersonDto personStub = new PersonDto();
+
     	personStub.setUnit(unitStub);
-    	personStub.setLogin(rstring);
-    	personStub.setPassword("test");
+    	personStub.setLogin("Test3");
+    	personStub.setPassword("Test");
+    	System.out.print(objectMapper.writeValueAsString(personStub));
     	
     	this.mockMvc.perform(post(mapping).contentType(MediaType.APPLICATION_JSON)
     			.content(objectMapper.writeValueAsString(personStub))
@@ -60,33 +92,42 @@ public class PersonControllerTest {
     }
     
     @Test
-    public void insertUpdateDelete() throws Exception{
-    	Random rand = new Random();
-    	String rstring = "test" + new Integer((rand.nextInt())).toString();
+    public void updatePersonTest() throws Exception{
+    	String name = "Testo";
     	PersonDto personStub = new PersonDto();
-    	OrganizationalUnitDto unitStub = new OrganizationalUnitDto();
-    	unitStub.setFullName("test");
-    	unitStub.setShortName("test");
-    	unitStub.setId((short) 1);
     	personStub.setUnit(unitStub);
-    	personStub.setLogin(rstring);
-    	personStub.setPassword("test");
+    	personStub.setLogin("Test2");
+    	personStub.setPassword("Test");
+    	personStub.setFirstName(name);
+    	personStub.setId(new Long(2));
     	
-    	this.mockMvc.perform(post(mapping).contentType(MediaType.APPLICATION_JSON)
-    			.content(objectMapper.writeValueAsString(personStub))
+    	this.mockMvc.perform(put(mapping).contentType(MediaType.APPLICATION_JSON).
+    			content(objectMapper.writeValueAsString(personStub))
     			)
     	.andExpect(status().isOk());
     	
+    	String result = this.mockMvc.perform(get(mapping+"/2")).andExpect(status().isOk())
+    	.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+    	.andReturn().getResponse().getContentAsString();
+    	
+    	PersonDto upPerson = objectMapper.readValue(result, PersonDto.class);
+    	if(!upPerson.getFirstName().equals(name)){
+    		fail("Update failed!");
+    	}
+    }
+    
+    @Test
+    public void getAllStudentsTest() throws Exception{
     	String results = this.mockMvc.perform(get(mapping))
     			.andExpect(status().isOk())
     			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-    			.andExpect(jsonPath("$[?(@.login=='"+rstring+"')]").isNotEmpty())
+    			.andExpect(jsonPath("$[?(@.login=='Test2')]").isNotEmpty())
     			.andReturn().getResponse().getContentAsString()
     			;
     	PersonDto[] persons = objectMapper.readValue(results, PersonDto[].class);
     	long id = 0;
     	for(PersonDto person: persons){
-    		if(person.getLogin().equals(rstring)){
+    		if(person.getLogin().equals("Test2")){
     			id = person.getId();
     			break;
     		}
@@ -94,24 +135,21 @@ public class PersonControllerTest {
     	if(id==0){
     		fail("Person not found!");
     	}
+    }
+    
+    @Test
+    public void deleteStudentTest() throws Exception{
+    	this.mockMvc.perform(delete(mapping+"/1")).andExpect(status().isNoContent());
     	
-    	rstring = "test" + new Integer((rand.nextInt())).toString();
-    	personStub.setId(id);
-    	personStub.setFirstName(rstring);
-    	this.mockMvc.perform(put(mapping).contentType(MediaType.APPLICATION_JSON).
-    			content(objectMapper.writeValueAsString(personStub))
-    			)
-    	.andExpect(status().isOk());
-    	
-    	String result = this.mockMvc.perform(get(mapping+"/"+new Long(id).toString())).andExpect(status().isOk())
-    	.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-    	.andReturn().getResponse().getContentAsString();
-    	
-    	PersonDto upPerson = objectMapper.readValue(result, PersonDto.class);
-    	if(!upPerson.getFirstName().equals(rstring)){
-    		fail("Update failed!");
+    	String results = this.mockMvc.perform(get(mapping))
+    			.andReturn().getResponse().getContentAsString()
+    			;
+    	PersonDto[] persons = objectMapper.readValue(results, PersonDto[].class);
+    	for(PersonDto person: persons){
+    		if(person.getLogin().equals("Test")){
+    			fail("Person found after delete!");
+    		}
     	}
-    	this.mockMvc.perform(delete(mapping+"/"+new Long(id).toString())).andExpect(status().isNoContent());
-    	
+    		
     }
 }
