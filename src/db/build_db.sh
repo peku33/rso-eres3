83f74e47ec1e38c40babd07f7aa86626caa48f30
Lukasz Kostyra <costyrra.xl@gmail.com>
2017-06-07 22:39:51 +0200

    Build DB and connect backend to DB by hostnames

--- src/db/build_db.sh
+++ src/db/build_db.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
 
+set -e
+
+NET_NAME="eres3-network"
 IMAGE_NAME="eres3-dbcluster"
-IP_PREFIX="192.168.103"
 
 declare -a ndb_mgmt_nodes=("man1.ndb" "man2.ndb")
-declare -a ndb_mgmt_nodes_ip=("201" "202")
 declare -a ndb_data_nodes=("data1.ndb" "data2.ndb")
-declare -a ndb_data_nodes_ip=("211" "212")
 declare -a sql_nodes=("node1.ndb" "node2.ndb" "node3.ndb")
-declare -a sql_nodes_ip=("221" "222" "223")
 
-echo "Initializing management nodes"
-
-for ((i=0; i < "${#ndb_mgmt_nodes[@]}"; ++i))
-do
-    docker run -d --net=${NET_NAME} --ip=${IP_PREFIX}.${ndb_mgmt_nodes_ip[$i]} --name=${ndb_mgmt_nodes[$i]} --hostname=${ndb_mgmt_nodes[$i]} $IMAGE_NAME ndb_mgmd
-done
 
 echo "Initializing data nodes"
 for ((i=0; i < "${#ndb_data_nodes[@]}"; ++i))
 do
-    docker run -d --net=${NET_NAME} --ip=${IP_PREFIX}.${ndb_data_nodes_ip[$i]} --name=${ndb_data_nodes[$i]} --hostname=${ndb_data_nodes[$i]} --link=${ndb_mgmt_nodes[0]}:${ndb_mgmt_nodes[0]} --link=${ndb_mgmt_nodes[1]}:${ndb_mgmt_nodes[1]} $IMAGE_NAME ndbd
+    docker create --net=${NET_NAME} --name=${ndb_data_nodes[$i]} --hostname=${ndb_data_nodes[$i]} $IMAGE_NAME ndbd
 done
-
-echo "Preparing DB for further use"
-docker run --name=datainit --hostname="data1.ndb" eres3-dbcluster /etc/mysql-init.sh
-docker rm datainit
 
 echo "Initializing mysql nodes"
 for ((i=0; i < "${#sql_nodes[@]}"; ++i))
 do
-    docker run -d --net=${NET_NAME} --ip=${IP_PREFIX}.${sql_nodes_ip[$i]} --name=${sql_nodes[$i]} --hostname=${sql_nodes[$i]} --link=${ndb_mgmt_nodes[0]}:${ndb_mgmt_nodes[0]} --link=${ndb_mgmt_nodes[1]}:${ndb_mgmt_nodes[1]} $IMAGE_NAME mysqld
+    docker create --net=${NET_NAME} --name=${sql_nodes[$i]} --hostname=${sql_nodes[$i]} $IMAGE_NAME mysqld
 done
+
+# create additional initalizer node
+docker create --net=${NET_NAME} --name=datainit --hostname=node1.ndb --net-alias=node1.ndb $IMAGE_NAME /etc/mysql-init.sh
+
+echo "Initializing management nodes"
+for ((i=0; i < "${#ndb_mgmt_nodes[@]}"; ++i))
+do
+    docker create --net=${NET_NAME} --name=${ndb_mgmt_nodes[$i]} --hostname=${ndb_mgmt_nodes[$i]} $IMAGE_NAME ndb_mgmd
+done
+
+
+#echo "Preparing DB for further use"
+echo "Starting data nodes"
+for ((i=0; i < "${#ndb_data_nodes[@]}"; ++i))
+do
+    docker start ${ndb_data_nodes[$i]}
+done
+
+echo "Starting mysql nodes"
+for ((i=1; i < "${#sql_nodes[@]}"; ++i))
+do
+    docker start ${sql_nodes[$i]}
+done
+
+docker start datainit
+
+echo "Starting management nodes"
+for ((i=0; i < "${#ndb_mgmt_nodes[@]}"; ++i))
+do
+    docker start ${ndb_mgmt_nodes[$i]}
+done
+
+# attach to datainit - when finished it will quit automatically
+docker attach datainit
+docker rm datainit
+docker start node1.ndb
---
+++ src/db/stop_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NETWORK_NAME="eres3-network"
+
+declare -a nodes=("node3.ndb" "node2.ndb" "node1.ndb" "data2.ndb" "data1.ndb" "man2.ndb" "man1.ndb")
+
+for i in "${nodes[@]}"
+do
+    docker stop $i
+done
